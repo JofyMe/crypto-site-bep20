79df2196289ee40601794a7cc58e70095fe1c8df
JofyMe <mr.tretyak15usa@gmail.com>
2023-04-12 08:27:14 +0300

    [V001] Made project structure, variables and project header

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require("gulp"),
+sass = require("gulp-sass")(require("sass")),
+autoprefixer = require("gulp-autoprefixer"),
+cleanCSS = require("gulp-clean-css"),
+rename = require("gulp-rename"),
+pug = require("gulp-pug"),
+browserSync = require("browser-sync").create();
+
+// reload
+gulp.task("browser-sync", function () {
+  browserSync.init({
+    server: {
+      baseDir: `app/`
+    },
+    notify: "false",
+    online: false,
+    tunnel: false,
+  });
+});
+
+gulp.task("sass", function () {
+  // task
+  return gulp
+    .src("app/sass/**/*.scss") // Путь к файлами
+    .pipe(sass().on("error", sass.logError)) // Вывод ошибок
+    .pipe(rename({ suffix: ".min", prefix: "" })) // rename
+    .pipe(cleanCSS()) // clean css
+    .pipe(autoprefixer({ browserlist: ["last 10 versions"], cascade: false })) // autoprefixer
+    .pipe(gulp.dest("app/css")) // Путь к папке с конечными файлами
+    .pipe(browserSync.reload({ stream: true }));
+  });
+
+gulp.task("js", function () {
+  return gulp.src("app/js/**/*.js")
+  .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('code', function () {
+  return gulp.src('app/**/*.html')
+    .pipe(browserSync.reload({ stream: true }))
+})
+
+// watch
+gulp.task("watch", function () {
+  // task
+  gulp.watch("app/sass/**/*.scss", gulp.parallel("sass"));
+  gulp.watch("app/js/**/*.js", gulp.parallel("js"));
+  gulp.watch('app/*.html', gulp.parallel('code'));
+});
+
+gulp.task(
+  "default",
+  gulp.parallel("sass", "js", "code", "browser-sync", "watch")
+  );
