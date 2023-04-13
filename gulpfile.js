f197204aa9de69218b25b3162bb39f6941b1ce85
JofyMe <mr.tretyak15usa@gmail.com>
2023-04-14 00:11:56 +0300

    [V004] Add about and motto blocks in main page

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,22 @@
 var gulp = require("gulp"),
-sass = require("gulp-sass")(require("sass")),
-autoprefixer = require("gulp-autoprefixer"),
-cleanCSS = require("gulp-clean-css"),
-rename = require("gulp-rename"),
-pug = require("gulp-pug"),
-browserSync = require("browser-sync").create();
-
+  sass = require("gulp-sass")(require("sass")),
+  autoprefixer = require("gulp-autoprefixer"),
+  cleanCSS = require("gulp-clean-css"),
+  rename = require("gulp-rename"),
+  pug = require("gulp-pug"),
+  browserSync = require("browser-sync").create();
+ 
 // reload
 gulp.task("browser-sync", function () {
   browserSync.init({
     server: {
-      baseDir: `docs/`
+      baseDir: `app/`,
     },
     notify: "false",
-    online: false,
     tunnel: false,
   });
 });
-
+ 
 gulp.task("sass", function () {
   // task
   return gulp
@@ -28,27 +27,32 @@ gulp.task("sass", function () {
     .pipe(autoprefixer({ browserlist: ["last 10 versions"], cascade: false })) // autoprefixer
     .pipe(gulp.dest("app/css")) // Путь к папке с конечными файлами
     .pipe(browserSync.reload({ stream: true }));
-  });
-
-gulp.task("js", function () {
-  return gulp.src("app/js/**/*.js")
-  .pipe(browserSync.reload({ stream: true }));
 });
-
-gulp.task('code', function () {
-  return gulp.src('app/**/*.html')
-    .pipe(browserSync.reload({ stream: true }))
-})
-
+ 
+gulp.task("js", function () {
+  return gulp
+    .src("app/js/**/*.js")
+    .pipe(gulp.dest("docs/js"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+ 
+gulp.task("code", function () {
+  return gulp
+    .src("app/**/*.html")
+    .pipe(gulp.dest("docs"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+ 
 // watch
 gulp.task("watch", function () {
   // task
   gulp.watch("app/sass/**/*.scss", gulp.parallel("sass"));
   gulp.watch("app/js/**/*.js", gulp.parallel("js"));
-  gulp.watch('app/*.html', gulp.parallel('code'));
+  gulp.watch("app/*.html", gulp.parallel("code"));
 });
-
+ 
 gulp.task(
   "default",
   gulp.parallel("sass", "js", "code", "browser-sync", "watch")
-  );
+);
+ 
