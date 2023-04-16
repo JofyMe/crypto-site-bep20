5b3cdbc51beccb5a401eb2ba08547a889f8bf509
JofyMe <mr.tretyak15usa@gmail.com>
2023-04-16 23:29:09 +0300

    [V009] Create footer and start adaptation

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,21 @@
 var gulp = require("gulp"),
-  sass = require("gulp-sass")(require("sass")),
-  autoprefixer = require("gulp-autoprefixer"),
-  cleanCSS = require("gulp-clean-css"),
-  rename = require("gulp-rename"),
-  browserSync = require("browser-sync").create();
+sass = require("gulp-sass")(require("sass")),
+autoprefixer = require("gulp-autoprefixer"),
+cleanCSS = require("gulp-clean-css"),
+rename = require("gulp-rename"),
+pug = require("gulp-pug"),
+browserSync = require("browser-sync").create();
 
 // reload
 gulp.task("browser-sync", function () {
   browserSync.init({
     server: {
-      baseDir: `app/`,
+      baseDir: `app/`
     },
     notify: "false",
+    online: false,
     tunnel: false,
   });
-});
-
-gulp.task("img", function () {
-  return gulp
-    .src("app/img/**/*")
-    .pipe(gulp.dest("docs/img"))
-    .pipe(browserSync.reload({ stream: true }));
 });
 
 gulp.task("sass", function () {
@@ -31,34 +26,29 @@ gulp.task("sass", function () {
     .pipe(rename({ suffix: ".min", prefix: "" })) // rename
     .pipe(cleanCSS()) // clean css
     .pipe(autoprefixer({ browserlist: ["last 10 versions"], cascade: false })) // autoprefixer
-    .pipe(gulp.dest("docs/css")) // Путь к папке с конечными файлами
+    .pipe(gulp.dest("app/css")) // Путь к папке с конечными файлами
     .pipe(browserSync.reload({ stream: true }));
-});
+  });
 
 gulp.task("js", function () {
-  return gulp
-    .src("app/js/**/*.js")
-    .pipe(gulp.dest("docs/js"))
-    .pipe(browserSync.reload({ stream: true }));
+  return gulp.src("app/js/**/*.js")
+  .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task("code", function () {
-  return gulp
-    .src("app/**/*.html")
-    .pipe(gulp.dest("docs"))
-    .pipe(browserSync.reload({ stream: true }));
-});
+gulp.task('code', function () {
+  return gulp.src('app/**/*.html')
+    .pipe(browserSync.reload({ stream: true }))
+})
 
 // watch
 gulp.task("watch", function () {
   // task
   gulp.watch("app/sass/**/*.scss", gulp.parallel("sass"));
   gulp.watch("app/js/**/*.js", gulp.parallel("js"));
-  gulp.watch("app/*.html", gulp.parallel("code"));
-  gulp.watch("app/img", gulp.parallel("img"));
+  gulp.watch('app/*.html', gulp.parallel('code'));
 });
 
 gulp.task(
   "default",
-  gulp.parallel("sass", "js", "code", "img", "browser-sync", "watch")
-);
+  gulp.parallel("sass", "js", "code", "browser-sync", "watch")
+  );
