6536e32da293cf2e26e062675abb07c7e458237c
JofyMe <mr.tretyak15usa@gmail.com>
2023-04-21 02:00:11 +0300

    [V013] Optimize page loading from 5.54 seconds to 1.2 seconds, and create a page preloader

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector(".preloader").classList.add("hidden");
+  }, 1500);
+};
+document.querySelector('.preloader').style.opacity = ".05"
