33799519b755a42e81c82aeac8e01c11b3e86692
JofyMe <mr.tretyak15usa@gmail.com>
2023-04-23 04:11:58 +0300

    [015] Create question page and adaptation page

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,13 @@ document.querySelector('.header__burger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.header__navbar').classList.toggle('open')
 })
+
+const headers = document.querySelectorAll("[data-name='spoiler-title']");
+
+headers.forEach(function (item) {
+  item.addEventListener("click", headerClick);
+});
+
+function headerClick() {
+  this.nextElementSibling.classList.toggle("spoiler-body");
+}
